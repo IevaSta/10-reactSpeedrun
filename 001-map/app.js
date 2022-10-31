d856e07edcb3217b99862eca0e18ae5090a37b2f
IevaSta <ieva@kontraforma.lt>
2022-10-31 22:55:58 +0200

    random and map

---
+++ 001-map/app.js
@@ -0,0 +1,43 @@
+console.log('Contenct');
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// console.log(rand(5, 10));
+
+
+//Sukurti masyva penkiu elementu, kuriuose butu random skaiciai nuo...iki
+const mas = [...Array(5)].map(_ => rand(100, 999));
+console.log(mas);
+
+const fe = mas.forEach(e => 1);
+const map = mas.map(e => e + 1);
+
+console.log(fe);
+console.log(map);
+
+
+
+
+console.log('------------------------');
+
+const cats = [
+    { name: 'Pilkis', weight: 4 },
+    { name: 'Murka', weight: 7 },
+    { name: 'Keris', weight: 3 }
+];
+
+//padidinti cats svori +1
+const cats2 = cats.map(c => ({ ...c, weight: c.weight + 1 }));
+console.log(cats2);
+
+//katinukai random svorio nuo 3 iki 10
+const cats3 = cats.map(c => ({ ...c, weight: rand(3, 10) }));
+console.log(cats3);
+
+//pridedi po papildomą savybę takesPill kuri atsitiktiniu būdu yra arba 'yes' arba 'no'
+const cats4 = cats.map(c => ({ ...c, takesPill: rand(0, 1) ? 'yes' : 'no' }));
+console.log(cats4);
+
